caf870e6937f2b2005dea254c75eb5cd9a523b66
Jonathan Hsu <jonhsu.design@gmail.com>
2020-07-27 13:42:49 -0700

    fixed resource links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='about'>
-            <p><a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>Conway's Game of Life</a> is a zero-player game that simulates life-cycles using cellular automaton. The game is played on a grid of any size, which is populated by cells. The game allows for interaction during the initial setup and will then run on its own until all life-cycles are completed. The Game of Life is <a href='https://stackoverflow.com/questions/7284/what-is-turing-complete'>Turing Complete</a></p>
+            <p><a target='_blank' href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>Conway's Game of Life</a> is a zero-player game that simulates life-cycles using cellular automaton. The game is played on a grid of any size, which is populated by cells. The game allows for interaction during the initial setup and will then run on its own until all life-cycles are completed. The Game of Life is <a target='_blank' href='https://stackoverflow.com/questions/7284/what-is-turing-complete'>Turing Complete</a></p>
 
             <div>
                 <p>There are four main rules to Conway's Game of Life:</p>
